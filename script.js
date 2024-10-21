844af6fc3643f4662ae17867275841b8f268f9c9
MONK <169875752+DevNChill@users.noreply.github.com>
2024-10-21 13:44:11 +0530

    working on step3 again removed index method as i'll have to anyways refresh it

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 "use strict";
 
 //Global Variables
+let index = 0;
 //This will help to extract only the last added book to myLibrary everytime function is called
-let i = 0;
 
 //This will select the main tag of html inside of body
 const MAIN = document.querySelector("main");
@@ -24,8 +24,8 @@ function Book(title, author, noOfPages, readingStatus, index) {
 }
 
 //This function will create objects using above defined object constructors and store them in myLibrary array
-function addBookToLibrary(title, author, noOfPages, readingStatus) {
-  let book = new Book(title, author, noOfPages, readingStatus);
+function addBookToLibrary(title, author, noOfPages, readingStatus, index) {
+  let book = new Book(title, author, noOfPages, readingStatus, index);
   myLibrary.push(book);
 }
 
@@ -33,43 +33,42 @@ function addBookToLibrary(title, author, noOfPages, readingStatus) {
 //function to loop through the array and display each book on the page
 function displayBooks() {
   //accesing each object(book) from the array myLibrary
-  //myLibrary.forEach((item) => {
-  let item = myLibrary[i];
-  console.log(item);
-  //creating a container which will store all details about the book
-  let itemBox = document.createElement("div");
+  myLibrary.forEach((item) => {
+    console.log(item);
+    //creating a container which will store all details about the book
+    let itemBox = document.createElement("div");
 
-  //dividing this box into further 4 rows each row will contain info about book like author title ect
-  //div that will serve as first row of container
-  let titleinfo = document.createElement("div");
-  titleinfo.textContent = `Title : ${item.title}`;
+    //dividing this box into further 4 rows each row will contain info about book like author title ect
+    //div that will serve as first row of container
+    let titleinfo = document.createElement("div");
+    titleinfo.textContent = `Title : ${item.title}`;
 
-  //div that will serve as first row of container
-  let authorinfo = document.createElement("div");
-  authorinfo.textContent = `Author : ${item.author}`;
+    //div that will serve as first row of container
+    let authorinfo = document.createElement("div");
+    authorinfo.textContent = `Author : ${item.author}`;
 
-  //div that will serve as third row of container
-  let pagesinfo = document.createElement("div");
-  pagesinfo.textContent = `Pages : ${item.noOfPages}`;
+    //div that will serve as third row of container
+    let pagesinfo = document.createElement("div");
+    pagesinfo.textContent = `Pages : ${item.noOfPages}`;
 
-  //div that will serve as fourth row of container
-  let statusinfo = document.createElement("div");
-  statusinfo.textContent = `Status : ${item.readingStatus}`;
+    //div that will serve as fourth row of container
+    let statusinfo = document.createElement("div");
+    statusinfo.textContent = `Status : ${item.readingStatus}`;
 
-  //Adding class "item-box" to container element which contains details about book
-  itemBox.classList.add("item-box");
+    //Adding class "item-box" to container element which contains details about book
+    itemBox.classList.add("item-box");
 
-  //Adding all it's children
-  itemBox.appendChild(titleinfo);
-  itemBox.appendChild(authorinfo);
-  itemBox.appendChild(pagesinfo);
-  itemBox.appendChild(statusinfo);
+    //Adding all it's children
+    itemBox.appendChild(titleinfo);
+    itemBox.appendChild(authorinfo);
+    itemBox.appendChild(pagesinfo);
+    itemBox.appendChild(statusinfo);
 
-  //adding container inside main
-  MAIN.appendChild(itemBox);
-  i++;
-  //});
+    //adding container inside main
+    MAIN.appendChild(itemBox);
+  });
 }
+function refresh() {}
 
 //adding event listener to add button so that form appears when user clicks on it
 ADDBUTTON.addEventListener("click", () => {
@@ -91,8 +90,10 @@ document.getElementById("submit-button").addEventListener("click", () => {
   }
 
   //Adding book created to the library
-  addBookToLibrary(title, author, noOfPages, readingStatus);
+  addBookToLibrary(title, author, noOfPages, readingStatus, index);
+  index++;
 
+  refresh();
   //display the book on main section of page
   displayBooks();
 
